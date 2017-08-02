bdffdebd4fe2d3ed4c0dcb33960126d572c4b0c0
Jacek Dudek <jacek.dudek@urbantide.com>
2017-08-02 11:05:23 +0100

    added missing test

---
+++ tests/test_no_file.cpp
@@ -0,0 +1,19 @@
+
+
+
+#include <iostream>
+// #include ""
+#include "processing/analyser.hpp"
+
+int main(int argc, char const *argv[])
+{
+	const char* input  = "./tests/sample_files/_______________.txt";
+	const char* output = "./tests/sample_files/temp_output.txt";
+
+	int result = analyse(input, output);
+	if( result != 0 ){
+		std::cout << "Passed" << std::endl;
+	}
+
+	return 0;
+}
